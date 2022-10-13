60cbef96587455e4e8d568915e457e9b2ae3f6ae
naderalattar <55719361+naderalattar@users.noreply.github.com>
2022-10-13 16:10:41 +0200

    Update Application.java

--- src/threadPool/Application.java
+++ src/threadPool/Application.java
@@ -7,54 +7,26 @@ import java.util.concurrent.Executors;
 
 public class Application {
     public static void main(String[] args) {
-//        ExecutorService executor= Executors.newFixedThreadPool(3);
-//
-//        executor.execute(new QuestionProcessor(22));
-//        executor.execute(new QuestionProcessor(33));
-//        executor.execute(new QuestionProcessor(44));
-//
-//        executor.shutdown();
-//
-//        String s="dsc";
-//        HashMap<String,String> map=new HashMap<>();
-//        LinkedHashMap<String,String> map1=new LinkedHashMap<>();
-//
-//        while (!executor.isTerminated()){
-//
-//        }
-//
-//        System.out.println("finished ------------------ ");
+       ExecutorService executor= Executors.newFixedThreadPool(3);
 
-        HashSet<Integer> hashSet=new HashSet<>();
-        hashSet.add(66);
-        hashSet.add(12);
-        hashSet.add(6);
-        hashSet.add(77);
-        hashSet.add(1);
+       executor.execute(new QuestionProcessor(22));
+       executor.execute(new QuestionProcessor(33));
+       executor.execute(new QuestionProcessor(44));
 
-        System.out.println(hashSet);
-        System.out.println();
-        LinkedList<Integer> dd=new LinkedList<>(hashSet);
-        Collections.sort(dd);
+       executor.shutdown();
 
-        System.out.println(dd);
+       String s="dsc";
+       HashMap<String,String> map=new HashMap<>();
+       LinkedHashMap<String,String> map1=new LinkedHashMap<>();
 
+       while (!executor.isTerminated()){
+
+       }
+
+       System.out.println("finished ------------------ ");
 
 
     }
 
 }
-class Emloyee implements Comparable {
-     int id;
-     String name;
-     public Emloyee(int id,String name){
-         this.id=id;
-         this.name=name;
 
-     }
-
-    @Override
-    public int compareTo(Object o) {
-        return 0;
-    }
-}
